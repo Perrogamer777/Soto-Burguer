8fd73c8c11f8dbc5e8b28add530266ca3dd7b7af
alex-bq <107068804+alex-bq@users.noreply.github.com>
2023-05-16 02:01:36 -0400

    El MEJOR COMIT DE LA PAGINA

--- javascript/funciones.js
+++ javascript/funciones.js
@@ -1,3 +1,13 @@
+$(document).ready(function () {
+  irArriba();
+  $("#alertRojo").hide();
+  $("#alertVerde").hide();
+
+
+});
+
+
+
 // Ventana modal
 var modal = document.getElementById("ventanaModal");
 
@@ -48,3 +58,31 @@ function initMap() {
     map: map,
   });
 }
+
+
+// validar correo
+function validarCorreo (correo){
+  var expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+  var esValido= expReg.test(correo);
+
+  if(esValido==true){
+    $("#alertVerde").show();
+    $("#alertRojo").hide();
+
+  }
+  else{
+    $("#alertRojo").show();
+    $("#alertVerde").hide();
+  }
+
+}
+
+//funcion ir arriba
+function irArriba(){
+  $('.ir-arriba').click(function(){ $('body,html').animate({ scrollTop:'0px' },1000); });
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 0){ $('.ir-arriba').slideDown(600); }else{ $('.ir-arriba').slideUp(600); }
+  });
+  $('.ir-abajo').click(function(){ $('body,html').animate({ scrollTop:'1000px' },1000); });
+}
+
