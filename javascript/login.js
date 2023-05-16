0d2a5535d84cb19481567970115b5cd49dd1764f
alex-bq <107068804+alex-bq@users.noreply.github.com>
2023-05-15 22:47:31 -0400

    comit

--- javascript/login.js
+++ javascript/login.js
@@ -1,23 +1,3 @@
-// function login (){
-//     var user, password
-
-//     user = document.getElementById("usuario").value;
-//     password = document.getElementById("contraseña").value;
-
-//     if ( user == "admin" && password == "1234") {
-//         alert("Inicio de sesión correcto")
-//         window.location = "menu.html";
-        
-//     } else {
-//         alert ("Datos Incorrectos")
-//     }
-// }
-
-
-
-
-
-
 function openLoginForm(){
     document.body.classList.add("showLoginForm");
 }
@@ -25,5 +5,10 @@ function openLoginForm(){
 function closeLoginForm(){
     document.body.classList.remove("showLoginForm");
 }
+
+
+
+
+
 
 
